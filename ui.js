873d60075c54566bc088b5b743fc33321fffd4bc
theplayernumberzero <bxkuygulamalar.swe@gmail.com>
2024-01-08 22:59:27 +0300

    repo infos

--- ui.js
+++ ui.js
@@ -65,4 +65,31 @@ class UI{
             div.remove();   //Oluşturduğumuz div i arayüzden kaldırma
         },2000)
     }
+
+    showRepoInfo(repos){
+        this.repoDiv.innerHTML = "";    //fonksiyon başında repo bilgilerini temizleme
+
+        repos.forEach(repo => {
+            this.repoDiv.innerHTML += `
+            <div class="mb-2 card-body">
+            <div class="row">
+                <div class="col-md-2">
+                <a href="${repo.html_url}" target = "_blank" id = "repoName">${repo.name}</a>
+                </div>
+                <div class="col-md-6">
+                    <button class="btn btn-secondary">
+                        Starlar  <span class="badge badge-light" id="repoStar">${repo.stargazers_count}</span>
+                    </button>
+
+                    <button class="btn btn-info">
+                        Forklar  <span class="badge badge-light" id ="repoFork">${repo.forks_count}</span>
+                    </button>
+            
+                </div>
+        </div>
+
+        </div>
+            `;
+        });
+    }
 }
